a0dfd27fda71625fb8b498000c51c8b71ebc2243
ilyuzaaaaa <61176809+ilyuzaaaaa@users.noreply.github.com>
2021-10-01 19:12:38 +0300

    Add files via upload

---
+++ 1/file.js
@@ -0,0 +1,46 @@
+function bresenham_line(context, x_start, y_start, x_end, y_end, colour) {
+
+    console.log(x_end, y_end);
+    ctx.fillStyle = colour;
+    let dx = x_end - x_start;
+    let dy = y_end - y_start;
+
+    let sign_x = Math.sign(dx);
+    let sign_y = Math.sign(dy);
+
+    let x = x_start;
+    let y = y_start;
+    let EPS = 0;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        while (x !== x_end) {
+            EPS += 2 * dy;
+            if (Math.abs(EPS) >= Math.abs(dx)) {
+                y += sign_y;
+                EPS -= 2 * sign_x * sign_y * dx;
+            }
+            context.fillRect(x, y, 1, 1);
+            x += sign_x;
+        }
+    } else {
+        while (y !== y_end) {
+            EPS += 2 * dx;
+            if (Math.abs(EPS) >= Math.abs(dy)) {
+                EPS -= 2 * sign_x * sign_y * dy;
+                x += sign_x;
+            }
+            context.fillRect(x, y, 1, 1);
+            y += sign_y;
+        }
+    }
+}
+function rotation(context, x_start, y_start, radius) {
+    let alpha = ((new Date()).getSeconds() * 6 - 90) % 361;
+    console.log("alpha=" + alpha);
+    let x_end = Math.abs(Math.trunc(x_start + radius * Math.cos(alpha * Math.PI / 180)));
+    let y_end = Math.abs(Math.trunc(y_start + radius * Math.sin(alpha * Math.PI / 180)));
+    bresenham_line(context, x_start, y_start, x_end, y_end, '#1fc731');
+    setInterval(function () {
+        bresenham_line(context, x_start, y_start, x_end, y_end, '#ffffff');
+    }, 1000);
+}
